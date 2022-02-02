8a2b93da2dbb81cd66bdd375f0e3501505320bb9
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-02-02 10:58:49 +0530

    done

---
+++ infinite-scroll/scroll.js
@@ -0,0 +1,38 @@
+let container = document.getElementById("scroll-data");
+
+var page = 1;
+
+const getData = () => {
+    fetch(`http://localhost:3001/data?_page=${page}&_limit=15`)
+        .then((data) => data.json())
+        .then((res) => {
+            console.log(res);
+            console.log(page);
+            showData(res)
+        })
+    
+}
+getData()
+
+const showData=(data)=> {
+    
+    data.forEach((d) => {
+        let name = document.createElement('h1');
+        name.innerText = d.title;
+        container.append(name);
+    })
+}
+
+const infiniteScroll = () => {
+    setTimeout(() => {
+        page++;
+        getData();
+    }, 500);
+}
+
+window.addEventListener('scroll', () => {
+    const {scrollHeight, scrollTop, clientHeight} = document.documentElement;
+    if (scrollHeight + scrollTop >= clientHeight) {
+        infiniteScroll();
+    }
+});
