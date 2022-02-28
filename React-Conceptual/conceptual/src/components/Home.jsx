4e72ac24053914ce5dea1eae0071c45d669b6eb2
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-02-28 14:50:07 +0530

    done

--- React-Conceptual/conceptual/src/components/Home.jsx
+++ React-Conceptual/conceptual/src/components/Home.jsx
@@ -1,14 +1,31 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { useState, useEffect} from "react";
+
 
 
 export const Home = () => {
-    const { token, handleToken } = useContext(AuthContext);
+    const [set, setData] = useState([]);
+
+    useEffect(() => {
+        getData()
+    },[])
+
+
+    async function getData() {
+        let res = await fetch("http://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian");
+        let data = await res.json();
+        setData(data.meals);
+        console.log(data);
+    }
 
     return (
-        <div>
-            <h1>HomePage</h1>
-            <button onClick={handleToken}>{token?"Sign-In":"Sign-Out"}</button>
-        </div>
+        <>
+            {
+                set.map((e) => (
+                    <div key={e.idMeal}>
+                        <img src={e.strMealThumb} alt=""/>
+                    </div>
+                ))
+           }
+        </>
     )
 }
--- React-Conceptual/conceptual/src/index.js
+++ React-Conceptual/conceptual/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthContextProvider } from './context/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthContextProvider>
       <App />
-    </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
