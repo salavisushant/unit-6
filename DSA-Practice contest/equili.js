84285b247b6bce6809e57322e799717fcc883f07
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-03-06 19:51:26 +0530

    done

---
+++ DSA-Practice contest/equili.js
@@ -0,0 +1,32 @@
+function equiElement(arr, n) {
+  
+}
+
+function runProgram(input) {
+    input = input.trim().split("\n");
+    var n = +input[0];
+    var arr = input[1].trim().split(" ").map(Number);
+    equiElement(arr, n)
+}
+
+if (process.env.USERNAME === "salav") {
+runProgram(`5
+3 3 5 5 1`);
+} else {
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+});
+process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0) ;
+});
+}
