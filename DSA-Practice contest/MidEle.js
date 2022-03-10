ebcf4dc2fb6de044a3be5820bc9459499323f0c7
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-03-10 11:15:28 +0530

    Create MidEle.js

---
+++ DSA-Practice contest/MidEle.js
@@ -0,0 +1,30 @@
+function runProgram(input) {
+    input = input.trim().split("\n");
+    var n = +input[0];
+    var arr = input[1].trim().split(" ").map(Number);
+
+    mid =Math.ceil(0 + arr.length/2);
+    
+}
+
+if (process.env.USERNAME === "salav") {
+runProgram(`5
+4 3 6 7 8`);
+} else {
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+});
+process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0) ;
+});
+}
