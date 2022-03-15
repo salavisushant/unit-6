eb414e51715cb4235068f167f58a5cc494022749
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-03-15 09:42:56 +0530

    Update PrimeCheckMatrix.js

--- DSA-Practice contest/PrimeCheckMatrix.js
+++ DSA-Practice contest/PrimeCheckMatrix.js
@@ -11,21 +11,47 @@ function isPrime(n){
     return false
 }
 
-var N = 3;
-var M = 3;
 
-var arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
+function runProgram(input) {
+    input = input.trim().split("\n");
+    var [n, m] = input[0].trim().split(" ").map(Number);
+    var line = 1;
+    var matrix = [];
 
-let count = 0;
-for (var i = 0; i < N; i++) {
-    for (var j = 0; j < M; j++) {
-        if (isPrime(arr[i][j])) {
-            count++
+    for (var i = 0; i < n; i++){
+        matrix.push(input[line++].trim().split(" ").map(Number));
+    }
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < m; j++) {
+            if (isPrime(matrix[i][j])) {
+                count++
+            }
         }
     }
+    console.log(count);
+
 }
-console.log(count);
+
+if (process.env.USERNAME === "salav") {
+runProgram(`3 3
+1 2 3 
+4 5 6
+7 8 9`);
+} else {
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+});
+process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0) ;
+});
+}
